6552c5e85c82e85817c96ef21606c8f347947cd5
Alexey Kutepov <alkutepov@gmail.com>
2014-10-07 22:43:31 +0400

    added spring

--- src/main/java/phone/book/model/Person.java
+++ src/main/java/phone/book/model/Person.java
@@ -1,10 +1,52 @@
 package phone.book.model;
 
-import javax.persistence.Entity;;
+import javax.persistence.*;
 
 /**
  * Created by alexey on 07.10.14.
  */
 @Entity
+@Table
 public class Person {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    private String phone;
+
+    public Person() {
+        super();
+    }
+
+    public Person(String name, String phone) {
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 }
