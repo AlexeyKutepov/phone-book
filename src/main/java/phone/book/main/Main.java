83a37871313a11512d8ba10520b043f586e90aa6
Alexey Kutepov <alkutepov@gmail.com>
2014-10-08 00:18:25 +0400

    added application context

---
+++ src/main/java/phone/book/main/Main.java
@@ -0,0 +1,33 @@
+package phone.book.main;
+
+import phone.book.dao.PersonDao;
+import phone.book.model.Person;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.List;
+
+/**
+ * Created by alexey on 07.10.14.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+
+        PersonDao personDAO = context.getBean(PersonDao.class);
+
+        Person person = new Person("alexey", "89200868942");
+
+        personDAO.save(person);
+
+        System.out.println("Person::"+person);
+
+        List<Person> list = personDAO.getPersonList();
+
+        for(Person p : list){
+            System.out.println("Person List::"+p);
+        }
+        //close resources
+        context.close();
+    }
+}
